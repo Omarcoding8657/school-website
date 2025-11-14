40c168bdccdcd2a295309c639f8f4869946f715b
Omarcoding8657 <your-email@example.com>
2025-11-13 21:50:06 -0600

    Fix template name

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def allow_cors(resp):
 # ---------------------- Web Pages ----------------------
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/game")
 def game():
